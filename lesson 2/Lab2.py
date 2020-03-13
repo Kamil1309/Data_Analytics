1370fd24530ae5692654f00a3a8df95185fe5127
STUDIES <kamil.szostek97@gmail.com>
2020-03-14 00:32:59 +0100

    lab2.2

--- lesson 2/Lab2.py
+++ lesson 2/Lab2.py
@@ -10,7 +10,7 @@ np.random.seed(1)
 ### 1 ###
 def plot_histograms( *, mu, sigma ):
     x = mu + sigma * np.random.randn(10000)
-
+    print(x)
     #cumulative=False
     plt.subplot(3, 2, 1)
     plt.hist(x, 10, density=True)
@@ -18,12 +18,12 @@ def plot_histograms( *, mu, sigma ):
     plt.grid(True)
 
     plt.subplot(3, 2, 3)
-    plt.hist(x, 50, density=True)
+    plt.hist(x, 40, density=True)
     plt.ylabel('Probability')
     plt.grid(True)
 
     plt.subplot(3, 2, 5)
-    plt.hist(x, 200, density=True)
+    plt.hist(x, 100, density=True)
     plt.xlabel('Value')
     plt.grid(True)
     #cumulative=True
@@ -33,11 +33,11 @@ def plot_histograms( *, mu, sigma ):
     plt.grid(True)
 
     plt.subplot(3, 2, 4)
-    plt.hist(x, 50, density=True, cumulative=True)
+    plt.hist(x, 40, density=True, cumulative=True)
     plt.grid(True)
 
     plt.subplot(3, 2, 6)
-    plt.hist(x, 200, density=True, cumulative=True)
+    plt.hist(x, 100, density=True, cumulative=True)
     plt.xlabel('Value')
     plt.grid(True)
 
@@ -46,4 +46,47 @@ def plot_histograms( *, mu, sigma ):
 
 plot_histograms( mu= 50, sigma= 15)
 plot_histograms( mu= 100, sigma= 15)
-plot_histograms( mu= 100, sigma= 50)
+plot_histograms( mu= 100, sigma= 150)
+
+
+### 2 ###
+def plot_histograms_legit( *, mu, sigma ):
+    x = mu + sigma * np.random.randn(10000)
+    x = logit(x)
+    print(x)
+    #cumulative=False
+    plt.subplot(3, 2, 1)
+    plt.hist(x, 10, density=True)
+    plt.title('cumulative=False')
+    plt.grid(True)
+
+    plt.subplot(3, 2, 3)
+    plt.hist(x, 40, density=True)
+    plt.ylabel('Probability')
+    plt.grid(True)
+
+    plt.subplot(3, 2, 5)
+    plt.hist(x, 100, density=True)
+    plt.xlabel('Value')
+    plt.grid(True)
+    #cumulative=True
+    plt.subplot(3, 2, 2)
+    plt.hist(x, 10, density=True, cumulative=True)
+    plt.title('cumulative=True')
+    plt.grid(True)
+
+    plt.subplot(3, 2, 4)
+    plt.hist(x, 40, density=True, cumulative=True)
+    plt.grid(True)
+
+    plt.subplot(3, 2, 6)
+    plt.hist(x, 100, density=True, cumulative=True)
+    plt.xlabel('Value')
+    plt.grid(True)
+
+    plt.suptitle('Normal distribution, mu = %d, sigma = %d' %(mu, sigma))
+    plt.show()
+
+plot_histograms( mu= 50, sigma= 15)
+plot_histograms( mu= 100, sigma= 15)
+plot_histograms( mu= 100, sigma= 150)
